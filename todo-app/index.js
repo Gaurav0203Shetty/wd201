ad7e03ccca88c6ad986d0fbca8a85c016ba905a6
Gaurav Shetty <gauraviationphotography@gmail.com>
2024-09-24 11:13:53 +0530

    Added todo app

---
+++ todo-app/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello world");
+})
+
+app.listen(3000, () => {
+    console.log("Startedd listening on port 3000");
+})
